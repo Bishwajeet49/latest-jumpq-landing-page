34ee16386b40fc8dcae7d2b58b4b063a361b9b5b
Rohit Jiwade <rohitjiwade2019@gmail.com>
2025-01-06 15:50:17 +0530

    update icons

--- components/Features.tsx
+++ components/Features.tsx
@@ -31,26 +31,57 @@ export default function Features() {
                             </div>
                         </div>
 
-                        <div   className="relative grid overflow-hidden rounded-[1.25rem] bg-gray-100 p-1 dark:bg-gray-800/50 w-large " >
-                        
+                        <div className="relative grid overflow-hidden rounded-[1.25rem] bg-gray-100 p-1 dark:bg-gray-800/50 w-large " >
+
                             <Feature title="Intelligent Buyer Targeting and re-targeting" description="Track and engage with buyers inon real-time , Enjoy next-level customer profiling, combined with buyer preferences and insights .">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="m-auto h-6 w-6 text-gray-700 dark:text-white">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z"></path>
+                                <svg className="m-auto h-6 w-6 text-gray-700 dark:text-white" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g clip-path="url(#clip0_668_44)">
+                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M4.98041 4.98091C3.46512 6.4962 2.57143 8.80819 2.57143 11.9999C2.57143 15.1917 3.46512 17.5036 4.98041 19.0189C6.49571 20.5343 8.80771 21.428 11.9994 21.428C15.1912 21.428 17.5032 20.5343 19.0185 19.0189C20.5337 17.5036 21.4276 15.1917 21.4276 11.9999C21.4276 11.2898 22.0032 10.7142 22.7133 10.7142C23.4233 10.7142 23.999 11.2898 23.999 11.9999C23.999 15.665 22.9642 18.7098 20.8368 20.8372C18.7094 22.9646 15.6645 23.9994 11.9994 23.9994C8.33438 23.9994 5.28952 22.9646 3.16214 20.8372C1.03477 18.7098 0 15.665 0 11.9999C0 8.33488 1.03477 5.29 3.16214 3.16264C5.28952 1.03526 8.33438 0.000488281 11.9994 0.000488281C12.7095 0.000488281 13.2852 0.576123 13.2852 1.2862C13.2852 1.99629 12.7095 2.57191 11.9994 2.57191C8.80771 2.57191 6.49571 3.46562 4.98041 4.98091ZM11.5392 6.3226C11.6963 7.01508 11.2624 7.70383 10.5699 7.861C9.59099 8.08317 8.91496 8.53855 8.46818 9.16029C8.01091 9.79661 7.71412 10.7198 7.71412 11.9992C7.71412 13.5453 8.14499 14.5716 8.78604 15.2127C9.4271 15.8537 10.4534 16.2846 11.9995 16.2846C13.3416 16.2846 14.2914 15.9584 14.9315 15.461C15.5585 14.9737 16.0131 14.226 16.1971 13.1267C16.3143 12.4263 16.9771 11.9536 17.6774 12.0709C18.3778 12.1881 18.8505 12.8509 18.7332 13.5512C18.4613 15.176 17.7285 16.544 16.5094 17.4913C15.3034 18.4286 13.7485 18.856 11.9995 18.856C9.98013 18.856 8.22089 18.2841 6.96777 17.0309C5.71464 15.7778 5.14269 14.0186 5.14269 11.9992C5.14269 10.3322 5.53063 8.84167 6.37997 7.65972C7.23981 6.46317 8.49225 5.6957 10.0008 5.35334C10.6933 5.19617 11.382 5.63013 11.5392 6.3226ZM16.3883 9.42712L12.9087 12.9067C12.4066 13.4088 11.5926 13.4088 11.0905 12.9067C10.5884 12.4046 10.5884 11.5905 11.0905 11.0884L14.5717 7.60716C14.1883 6.08796 14.6288 4.47105 15.7447 3.3551L18.4644 0.635384C18.6859 0.413837 19.0073 0.324493 19.3114 0.399918C19.6156 0.475342 19.858 0.704516 19.9502 1.00391L20.6681 3.33178L22.996 4.04968C23.2954 4.14201 23.5245 4.38439 23.5999 4.68849C23.6753 4.99257 23.586 5.31394 23.3645 5.5355L20.645 8.25515C19.528 9.37223 17.909 9.81254 16.3883 9.42712Z" fill="url(#paint0_linear_668_44)" />
+                                    </g>
+                                    <defs>
+                                        <linearGradient id="paint0_linear_668_44" x1="-0.75" y1="11.7505" x2="24.75" y2="11.7505" gradientUnits="userSpaceOnUse">
+                                            <stop stop-color="#FAEC13" />
+                                            <stop offset="1" stop-color="#FE6129" />
+                                        </linearGradient>
+                                        <clipPath id="clip0_668_44">
+                                            <rect width="24" height="24" fill="white" />
+                                        </clipPath>
+                                    </defs>
                                 </svg>
+
                             </Feature>
 
-                      
+
 
                             <Feature title="Eliminate blindspots and rekindle opportunities" description="Gain real-time visibility of your entire pipeline, Integrate leads, track as they are being qualified & nurtured, and get involved when the time is right.">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="m-auto h-6 w-6 text-gray-700 dark:text-white">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"></path>
+                                <svg className="m-auto h-6 w-6 text-gray-700 dark:text-white" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g clip-path="url(#clip0_668_48)">
+                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M9.25 0.75C8.00735 0.75 7 1.75736 7 3C7 4.24264 8.00735 5.25 9.25 5.25C10.4927 5.25 11.5 4.24264 11.5 3C11.5 1.75736 10.4927 0.75 9.25 0.75ZM0.75 8C0.75 6.75735 1.75736 5.75 3 5.75C4.24264 5.75 5.25 6.75735 5.25 8C5.25 9.24265 4.24264 10.25 3 10.25C1.75736 10.25 0.75 9.24265 0.75 8ZM8.4595 6.81215C8.7184 6.77535 8.99955 6.75 9.284 6.75C9.5665 6.75 9.8464 6.775 10.1046 6.8114C11.0331 6.9422 11.629 7.75415 11.6946 8.6387C11.7873 9.8885 11.9148 11.7425 11.9347 12.745C11.9684 12.7464 12.0051 12.748 12.0449 12.75C12.2701 12.7613 12.5921 12.7839 12.9823 12.8289C13.7617 12.9188 14.8175 13.0988 15.9188 13.4599C17.1145 13.852 17.9208 14.9298 17.9828 16.1828C18.0566 17.6745 17.9307 19.9072 16.9352 22.1941C16.6431 22.865 16.0006 23.2929 15.2839 23.3524C14.448 23.4218 13.1134 23.5 11.3448 23.5C9.2315 23.5 8.0215 23.3883 7.4435 23.3126C7.1271 23.2711 6.8336 23.143 6.5862 22.9475C4.33496 21.1684 2.45344 18.1973 1.79219 17.0842C1.59262 16.7482 1.50876 16.3382 1.6197 15.9401C1.81592 15.2359 2.14081 14.7076 2.46753 14.3242C3.09257 13.5907 4.11509 13.6648 4.85076 14.1165C5.5134 14.5233 6.0606 14.8958 6.44255 15.1669C6.5149 15.2182 6.58135 15.266 6.6415 15.3096C6.65645 12.5499 6.7915 10.0655 6.88735 8.62805C6.94605 7.7477 7.5383 6.9432 8.4595 6.81215ZM21 5.75C22.2427 5.75 23.25 6.75735 23.25 8C23.25 9.24265 22.2427 10.25 21 10.25C19.7573 10.25 18.75 9.24265 18.75 8C18.75 6.75735 19.7573 5.75 21 5.75ZM13.25 8C13.25 6.75735 14.2573 5.75 15.5 5.75C16.7427 5.75 17.75 6.75735 17.75 8C17.75 9.24265 16.7427 10.25 15.5 10.25C14.2573 10.25 13.25 9.24265 13.25 8ZM23.25 3C23.25 1.75736 22.2427 0.75 21 0.75C19.7573 0.75 18.75 1.75736 18.75 3C18.75 4.24264 19.7573 5.25 21 5.25C22.2427 5.25 23.25 4.24264 23.25 3Z" fill="url(#paint0_linear_668_48)" />
+                                    </g>
+                                    <defs>
+                                        <linearGradient id="paint0_linear_668_48" x1="0.75" y1="12.125" x2="23.25" y2="12.125" gradientUnits="userSpaceOnUse">
+                                            <stop stop-color="#FAEC13" />
+                                            <stop offset="1" stop-color="#FE6129" />
+                                        </linearGradient>
+                                        <clipPath id="clip0_668_48">
+                                            <rect width="24" height="24" fill="white" />
+                                        </clipPath>
+                                    </defs>
                                 </svg>
+
                             </Feature>
 
                             <Feature title="Smooth integration" description="Simply upload your leads to JumpQ, activate and we’ll notify you only when your attention is required.">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="m-auto h-6 w-6 text-gray-700 dark:text-white">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
+                                <svg className="m-auto h-6 w-6 text-gray-700 dark:text-white" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M16.4337 6.00972L19.1025 3.34092L17.4565 1.69482L14.7876 4.36362L14.6878 4.26382C13.7787 3.35472 12.3047 3.35472 11.3957 4.26382L8.14488 7.51452L7.80398 7.85552V13.0932L5.81698 15.0801L4.99388 15.9033L5.81698 16.7263L6.44888 17.3582L4.17088 19.6362C3.95258 19.8544 3.65658 19.9771 3.34788 19.9771H0.238281V22.305H3.34788C4.27398 22.305 5.16208 21.9371 5.81698 21.2822L8.09488 19.0043L8.72678 19.6362L9.54988 20.4592L10.3729 19.6362L12.3599 17.6492H17.5976L17.9386 17.3083L21.1894 14.0575C22.0985 13.1483 22.0984 11.6744 21.1894 10.7653L21.0895 10.6655L23.7583 7.99672L22.1122 6.35062L19.4434 9.01942L16.4337 6.00972Z" fill="url(#paint0_linear_668_52)" />
+                                    <defs>
+                                        <linearGradient id="paint0_linear_668_52" x1="0.238281" y1="11.9999" x2="23.7583" y2="11.9999" gradientUnits="userSpaceOnUse">
+                                            <stop stop-color="#FAEC13" />
+                                            <stop offset="1" stop-color="#FE6129" />
+                                        </linearGradient>
+                                    </defs>
                                 </svg>
+
                             </Feature>
                         </div>
 
--- components/Solution.tsx
+++ components/Solution.tsx
@@ -14,7 +14,7 @@ export default function Solution() {
                 </div>
                 <div className="flex-row-reverse mt-16 md:mt-20  justify-between  text-gray-600 md:flex md:gap-6 lg:gap-12 lg:space-y-0">
                     <div className="relative ml-auto h-full md:w-1/2 ">
-                        <h2 className="text-xl font-bold text-gray-900 dark:text-white md:text-2xl lg:text-4xl text-primary" style={{marginLeft:"20px"}}  >After JumpQ</h2>
+                        <h2 className="text-xl font-bold text-gray-900 dark:text-white md:text-2xl lg:text-4xl text-primary" style={{ marginLeft: "20px" }}  >After JumpQ</h2>
                         <Timeline />
                         {/* <Image className="dark:hidden" src="/images/milestone.webp" alt="app milestone" width="1174" height="1134" />
                         <Image className="hidden dark:block" src="/images/milestone-dark.webp" alt="app milestone" width="1174" height="1134" /> */}
@@ -27,9 +27,26 @@ export default function Solution() {
                         <div className="space-y-4 divide-y divide-gray-100 dark:divide-gray-800">
                             <div className="flex gap-4 md:items-center">
                                 <div className="flex h-12 w-12 gap-4 rounded border border-gray-200 dark:border-gray-900">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="m-auto h-6 w-6 text-gray-700 dark:text-gray-300">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"></path>
+                                    <svg className="m-auto h-6 w-6 text-gray-700 dark:text-white" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M12 12C17.799 12 22.5 9.31371 22.5 6C22.5 2.68629 17.799 0 12 0C6.20101 0 1.5 2.68629 1.5 6C1.5 9.31371 6.20101 12 12 12Z" fill="url(#paint0_linear_668_56)" />
+                                        <path d="M20.541 12.1962C18.243 13.5093 15.2185 14.25 12 14.25C8.7815 14.25 5.75705 13.5093 3.45906 12.1962C2.76366 11.7988 2.0934 11.321 1.5 10.7666V12C1.5 15.3136 6.20101 18 12 18C17.799 18 22.5 15.3136 22.5 12V10.7666C21.9066 11.321 21.2364 11.7988 20.541 12.1962Z" fill="url(#paint1_linear_668_56)" />
+                                        <path d="M1.5 16.7666V18.0001C1.5 21.3137 6.20101 24.0001 12 24.0001C17.799 24.0001 22.5 21.3137 22.5 18.0001V16.7666C21.9066 17.3212 21.2364 17.7989 20.541 18.1963C18.243 19.5094 15.2185 20.2501 12 20.2501C8.7815 20.2501 5.75705 19.5094 3.45906 18.1963C2.76366 17.7989 2.0934 17.3212 1.5 16.7666Z" fill="url(#paint2_linear_668_56)" />
+                                        <defs>
+                                            <linearGradient id="paint0_linear_668_56" x1="1.5" y1="6" x2="22.5" y2="6" gradientUnits="userSpaceOnUse">
+                                                <stop stop-color="#FAEC13" />
+                                                <stop offset="1" stop-color="#FE6129" />
+                                            </linearGradient>
+                                            <linearGradient id="paint1_linear_668_56" x1="1.5" y1="14.3833" x2="22.5" y2="14.3833" gradientUnits="userSpaceOnUse">
+                                                <stop stop-color="#FAEC13" />
+                                                <stop offset="1" stop-color="#FE6129" />
+                                            </linearGradient>
+                                            <linearGradient id="paint2_linear_668_56" x1="1.5" y1="20.3833" x2="22.5" y2="20.3833" gradientUnits="userSpaceOnUse">
+                                                <stop stop-color="#FAEC13" />
+                                                <stop offset="1" stop-color="#FE6129" />
+                                            </linearGradient>
+                                        </defs>
                                     </svg>
+
                                 </div>
                                 <div className="w-5/6">
                                     <h3 className="text-lg font-semibold text-gray-700 dark:text-white">Multiple Databases Pics</h3>
@@ -39,10 +56,21 @@ export default function Solution() {
 
                             <div className="flex gap-4 pt-4 md:items-center">
                                 <div className="flex h-12 w-12 gap-4 rounded border border-gray-200 dark:border-gray-900">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="m-auto h-6 w-6 text-gray-700 dark:text-gray-300">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"></path>
+                                    <svg className="m-auto h-6 w-6 text-gray-700 dark:text-white" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <g clip-path="url(#clip0_668_61)">
+                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M18 3C19.5977 3 20.9037 4.24892 20.9949 5.82373L21 6V20C21 20.3623 20.8041 20.6962 20.4878 20.873C20.211 21.0276 19.8801 21.0407 19.595 20.9144L19.4759 20.8517L16.75 19.1742L14.0241 20.8517C13.7384 21.0274 13.3864 21.047 13.0859 20.9103L12.9759 20.8517L10.25 19.1742L7.5241 20.8517C7.21556 21.0415 6.82845 21.0497 6.5122 20.873C6.23108 20.7158 6.04503 20.4346 6.00715 20.1195L6 20V14H4C3.48717 14 3.06449 13.614 3.00673 13.1166L3 13V5.5C3 4.17452 4.03154 3.08996 5.33562 3.00532L5.5 3H18ZM15 12H11C10.4477 12 10 12.4477 10 13C10 13.5523 10.4477 14 11 14H15C15.5523 14 16 13.5523 16 13C16 12.4477 15.5523 12 15 12ZM5.5 5C5.22386 5 5 5.22386 5 5.5V12H6V5.5C6 5.22386 5.77614 5 5.5 5ZM16 8H11C10.4477 8 10 8.44772 10 9C10 9.51283 10.386 9.93551 10.8834 9.99327L11 10H16C16.5523 10 17 9.55228 17 9C17 8.48717 16.614 8.06449 16.1166 8.00673L16 8Z" fill="url(#paint0_linear_668_61)" />
+                                        </g>
+                                        <defs>
+                                            <linearGradient id="paint0_linear_668_61" x1="3" y1="12" x2="21" y2="12" gradientUnits="userSpaceOnUse">
+                                                <stop stop-color="#FAEC13" />
+                                                <stop offset="1" stop-color="#FE6129" />
+                                            </linearGradient>
+                                            <clipPath id="clip0_668_61">
+                                                <rect width="24" height="24" fill="white" />
+                                            </clipPath>
+                                        </defs>
                                     </svg>
+
                                 </div>
                                 <div className="w-5/6">
                                     <h3 className="text-lg font-semibold text-gray-700 dark:text-white">Pre-Sales Complexity</h3>
@@ -52,9 +80,16 @@ export default function Solution() {
 
                             <div className="flex gap-4 pt-4 md:items-center">
                                 <div className="flex h-12 w-12 gap-4 rounded border border-gray-200 dark:border-gray-900">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="m-auto h-6 w-6 text-gray-700 dark:text-gray-300">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"></path>
+                                    <svg className="m-auto h-6 w-6 text-gray-700 dark:text-white" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M12 22.7812C6.04568 22.7812 1.21875 17.9543 1.21875 12C1.21875 6.04568 6.04568 1.21875 12 1.21875C17.9543 1.21875 22.7812 6.04568 22.7812 12C22.7812 17.9543 17.9543 22.7812 12 22.7812ZM10.0833 6.48958C10.0833 5.43106 10.9415 4.57292 12 4.57292C13.0585 4.57292 13.9167 5.43106 13.9167 6.48958C13.9167 7.54811 13.0585 8.40625 12 8.40625C10.9415 8.40625 10.0833 7.54811 10.0833 6.48958ZM13.3802 10.6171C13.3465 10.0719 12.9976 9.64806 12.4522 9.61639C12.3208 9.60877 12.1707 9.60417 12 9.60417H10.3229C10.0424 9.60417 9.80805 9.61111 9.61313 9.6219C9.04455 9.65347 8.66476 10.0728 8.65053 10.6421C8.64756 10.7606 8.64583 10.8933 8.64583 11.0417C8.64583 11.19 8.64756 11.3227 8.65053 11.4412C8.66476 12.0105 9.04455 12.4299 9.61313 12.4614C9.80805 12.4722 10.0424 12.4792 10.3229 12.4792C10.4075 12.4792 10.4879 12.4785 10.5643 12.4773C10.5631 12.6296 10.5625 12.7898 10.5625 12.9583V15.8469C9.95401 15.8595 9.50004 15.8793 9.16707 15.8996C8.64291 15.9317 8.2222 16.2543 8.18349 16.778C8.17309 16.9189 8.16667 17.0823 8.16667 17.2708C8.16667 17.4594 8.17309 17.6227 8.18349 17.7637C8.2222 18.2874 8.64291 18.61 9.16707 18.642C9.72266 18.676 10.6151 18.7083 12 18.7083C13.3849 18.7083 14.2773 18.676 14.8329 18.642C15.3571 18.61 15.7778 18.2874 15.8165 17.7637C15.8269 17.6227 15.8333 17.4594 15.8333 17.2708C15.8333 17.0823 15.8269 16.9189 15.8165 16.778C15.7778 16.2543 15.3571 15.9317 14.8329 15.8996C14.5 15.8793 14.046 15.8595 13.4375 15.8469V12.9583C13.4375 11.8549 13.4107 11.1087 13.3802 10.6171Z" fill="url(#paint0_linear_668_66)" />
+                                        <defs>
+                                            <linearGradient id="paint0_linear_668_66" x1="1.21875" y1="12" x2="22.7812" y2="12" gradientUnits="userSpaceOnUse">
+                                                <stop stop-color="#FAEC13" />
+                                                <stop offset="1" stop-color="#FE6129" />
+                                            </linearGradient>
+                                        </defs>
                                     </svg>
+
                                 </div>
                                 <div className="w-5/6">
                                     <h3 className="text-lg font-semibold text-gray-700 dark:text-white">Product Information</h3>
@@ -65,8 +100,14 @@ export default function Solution() {
 
                             <div className="flex gap-4 pt-4 md:items-center">
                                 <div className="flex h-12 w-12 gap-4 rounded border border-gray-200 dark:border-gray-900">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="m-auto h-6 w-6 text-gray-700 dark:text-gray-300">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"></path>
+                                    <svg className="m-auto h-6 w-6 text-gray-700 dark:text-white" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M3 0C2.20435 0 1.44129 0.31607 0.87868 0.87868C0.31607 1.44129 0 2.20435 0 3L0 15C0 15.7956 0.31607 16.5587 0.87868 17.1213C1.44129 17.6839 2.20435 18 3 18H6.75C6.98287 18 7.21254 18.0542 7.42082 18.1584C7.6291 18.2625 7.81028 18.4137 7.95 18.6L10.8 22.3995C10.9397 22.5858 11.1209 22.737 11.3292 22.8411C11.5375 22.9453 11.7671 22.9995 12 22.9995C12.2329 22.9995 12.4625 22.9453 12.6708 22.8411C12.8791 22.737 13.0603 22.5858 13.2 22.3995L16.05 18.6C16.1897 18.4137 16.3709 18.2625 16.5792 18.1584C16.7875 18.0542 17.0171 18 17.25 18H21C21.7956 18 22.5587 17.6839 23.1213 17.1213C23.6839 16.5587 24 15.7956 24 15V3C24 2.20435 23.6839 1.44129 23.1213 0.87868C22.5587 0.31607 21.7956 0 21 0L3 0ZM12 5.9895C14.496 3.423 20.7375 7.914 12 13.6875C3.2625 7.9125 9.504 3.423 12 5.9895Z" fill="url(#paint0_linear_668_70)" />
+                                        <defs>
+                                            <linearGradient id="paint0_linear_668_70" x1="0" y1="11.4997" x2="24" y2="11.4997" gradientUnits="userSpaceOnUse">
+                                                <stop stop-color="#FAEC13" />
+                                                <stop offset="1" stop-color="#FE6129" />
+                                            </linearGradient>
+                                        </defs>
                                     </svg>
                                 </div>
                                 <div className="w-5/6">
@@ -78,9 +119,16 @@ export default function Solution() {
 
                             <div className="flex gap-4 pt-4 md:items-center">
                                 <div className="flex h-12 w-12 gap-4 rounded border border-gray-200 dark:border-gray-900">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="m-auto h-6 w-6 text-gray-700 dark:text-gray-300">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"></path>
+                                    <svg className="m-auto h-6 w-6 text-gray-700 dark:text-white" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M1.41406 0.240234H9.64606V8.47223H6.90686C7.39116 9.84253 8.69796 10.8242 10.2341 10.8242H14.3501V7.88423H22.5821V16.1162H14.3501V13.1762H10.2341C8.91036 13.1762 7.68876 12.7389 6.70606 12.0007V15.5282H9.64606V23.7602H1.41406V15.5282H4.35406V8.47223H1.41406V0.240234Z" fill="url(#paint0_linear_668_73)" />
+                                        <defs>
+                                            <linearGradient id="paint0_linear_668_73" x1="1.41406" y1="12.0002" x2="22.5821" y2="12.0002" gradientUnits="userSpaceOnUse">
+                                                <stop stop-color="#FAEC13" />
+                                                <stop offset="1" stop-color="#FE6129" />
+                                            </linearGradient>
+                                        </defs>
                                     </svg>
+
                                 </div>
                                 <div className="w-5/6">
                                     <h3 className="text-lg font-semibold text-gray-700 dark:text-white">Integrations </h3>
